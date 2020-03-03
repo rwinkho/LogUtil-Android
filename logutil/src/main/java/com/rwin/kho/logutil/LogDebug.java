b0c2de1d4462003b138c9228e34b95ff3746c745
Kho <kerwin.kho@nokia.com>
2020-03-04 01:07:01 +0800

    changing access to public

--- logutil/src/main/java/com/rwin/kho/logutil/LogDebug.java
+++ logutil/src/main/java/com/rwin/kho/logutil/LogDebug.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class LogDebug {
     private static final String TAG = "SUPER_AWESOME_APP";
-    private static void d(String message) {
+    public static void d(String message) {
         Log.d(TAG, message);
     }
 }
